36ceda0b587da33f70cc36f9b73aadb529ba6b21
Clement Escoffier <clement.escoffier@gmail.com>
2018-09-12 16:32:35 +0200

    Refactoring to use the new client

--- current-line-updater/src/main/java/com/redhat/coderland/reactica/currentline/CurrentLineUpdaterVerticle.java
+++ current-line-updater/src/main/java/com/redhat/coderland/reactica/currentline/CurrentLineUpdaterVerticle.java
@@ -1,28 +1,18 @@
 package com.redhat.coderland.reactica.currentline;
 
 import com.redhat.coderland.reactica.model.User;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.Future;
+import io.vertx.reactivex.CompletableHelper;
 import io.vertx.reactivex.core.AbstractVerticle;
+import me.escoffier.reactive.rhdg.AsyncCache;
+import me.escoffier.reactive.rhdg.DataGridClient;
+import me.escoffier.reactive.rhdg.DataGridConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.Search;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
-import org.infinispan.protostream.FileDescriptorSource;
-import org.infinispan.protostream.MessageMarshaller;
-import org.infinispan.protostream.SerializationContext;
-import org.infinispan.query.api.continuous.ContinuousQuery;
-import org.infinispan.query.api.continuous.ContinuousQueryListener;
 import org.infinispan.query.dsl.Query;
 import org.infinispan.query.dsl.QueryFactory;
 import org.infinispan.query.dsl.SortOrder;
-import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
-
-import java.io.*;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class CurrentLineUpdaterVerticle extends AbstractVerticle {
 
@@ -30,135 +20,45 @@ public class CurrentLineUpdaterVerticle extends AbstractVerticle {
   private static final String USER_PROTOBUFF_DEFINITION_FILE = "/user.proto";
   private static final String USEREVENTS_CACHENAME = "userevents";
 
-  private RemoteCache<String,User> cache;
-
-  final Map<String, User> matches = new ConcurrentHashMap<>();
-
-  ContinuousQueryListener<String, User> listener = new ContinuousQueryListener<String, User>() {
-    @Override
-    public void resultJoining(String key, User value) {
-      matches.put(key, value);
-      LOGGER.info("Received an new value for user with key " + key + " and value " + value);
-      vertx.eventBus().send("current-line-events", JsonObject.mapFrom(matches));
-    }
-
-    @Override
-    public void resultUpdated(String key, User value) {
-      matches.replace(key, value);
-      LOGGER.info("Received an updated value for user with key " + key + " and value " + value);
-      vertx.eventBus().send("current-line-events", JsonObject.mapFrom(matches));
-    }
-
-    @Override
-    public void resultLeaving(String key) {
-      matches.remove(key);
-      LOGGER.info("Received an removed value for user with key " + key);
-      vertx.eventBus().send("current-line-events", JsonObject.mapFrom(matches));
-    }
-  };
-
-
+  private AsyncCache<String, User> cache;
 
   @Override
-  public void start() throws Exception {
-    LOGGER.info("Staring deployment of " + this.getClass().getName());
-    ConfigurationBuilder cb = new ConfigurationBuilder();
-    cb.addServer()
-      .host(config().getString("host"))
-      .port(config().getInteger("port"))
-      .marshaller(new ProtoStreamMarshaller());
-    RemoteCacheManager cacheManager = new RemoteCacheManager(cb.build());
-    SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(cacheManager);
-    serCtx.registerProtoFiles(FileDescriptorSource.fromResources(USER_PROTOBUFF_DEFINITION_FILE));
-    serCtx.registerMarshaller(new MessageMarshaller<User>() {
-      @Override
-      public User readFrom(MessageMarshaller.ProtoStreamReader reader) throws IOException {
-        User user = new User();
-        user.setId(reader.readString("id"));
-        user.setName(reader.readString("name"));
-        user.setRideId(reader.readString("rideId"));
-        user.setCurrentState(reader.readString("currentState"));
-        user.setEnterTime(reader.readLong("enterTime"));
+  public void start(Future<Void> done) throws Exception {
+    LOGGER.info("Starting deployment of " + this.getClass().getName());
 
-        return user;
-      }
-
-      @Override
-      public void writeTo(MessageMarshaller.ProtoStreamWriter writer, User user) throws IOException {
-        writer.writeString("id",user.getId());
-        writer.writeString("name",user.getName());
-        writer.writeString("rideId","reactica");
-        writer.writeString("currentState",user.getCurrentState());
-        writer.writeLong("enterTime",user.getEnterTime());
-
-      }
-
-      @Override
-      public Class<? extends User> getJavaClass() {
-        return User.class;
-      }
-
-      @Override
-      public String getTypeName() {
-        return "com.redhat.coderland.reactica.model.User";
-      }
-    });
-
-    LOGGER.info("Successfully created a RemoteCacheManager");
-
-    this.cache = cacheManager.getCache(USEREVENTS_CACHENAME);
-
-    LOGGER.info("Successfully got the RemoteCache instance for " + USEREVENTS_CACHENAME);
-
-    // register the schemas with the server too
-
-    RemoteCache<String, String> metadataCache = cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-    metadataCache.putIfAbsent(USER_PROTOBUFF_DEFINITION_FILE, readResource(USER_PROTOBUFF_DEFINITION_FILE));
-    String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
-    if (errors != null) {
-      throw new IllegalStateException("Some Protobuff schema files contain errors:\n" + errors);
-    }
-
-    LOGGER.info("Successfully registered the schema " + USER_PROTOBUFF_DEFINITION_FILE + " in " + ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-
-    QueryFactory queryFactory = Search.getQueryFactory(this.cache);
-    Query query = queryFactory.from(User.class)
-      .having("rideId").eq("reactica")
-      .orderBy("enterTime", SortOrder.DESC)
-      .build();
-    ContinuousQuery<String, User> continuousQuery = Search.getContinuousQuery(this.cache);
-    continuousQuery.addContinuousQueryListener(query, listener);
-
-    LOGGER.info("Successfully connected the continuous query " + listener.getClass().getName());
-
-
+    DataGridClient.create(
+      vertx,
+      new DataGridConfiguration()
+        .setHost(config().getString("host"))
+        .setPort(config().getInteger("port"))
+        .addMarshaller(new ProtoStreamMarshaller())
+        .addProtoFile(USER_PROTOBUFF_DEFINITION_FILE, new UserMarshaller(), true)
+    )
+      .doOnSuccess(client -> {
+        LOGGER.info("Successfully created a Data Grid Client");
+      })
+      .flatMap(client -> client.<String, User>getCache(USEREVENTS_CACHENAME))
+      .doOnSuccess(cache -> {
+        LOGGER.info("Successfully got the cache {} ", USEREVENTS_CACHENAME);
+        this.cache = cache;
+      })
+      .flatMapCompletable(cache -> {
+        QueryFactory queryFactory = cache.getQueryFactory();
+        Query query = queryFactory.from(User.class)
+          .having("rideId").eq("reactica")
+          .orderBy("enterTime", SortOrder.DESC)
+          .build();
+        return cache.registerContinuousQuery(query, new UserContinuousQueryListener(vertx));
+      }).doOnComplete(() -> LOGGER.info("Successfully connected the continuous query"))
+      .subscribe(CompletableHelper.toObserver(done));
   }
 
   @Override
-  public void stop() {
-    if(this.cache != null) {
-      Search.getContinuousQuery(this.cache).removeContinuousQueryListener(listener);
-      this.cache.stop();
-    }
-  }
-
-
-  /**
-   * Note that this is a blocking call, but since are only reading this when we start the verticle once we will accept this as a blocking
-   * @param resourcePath
-   * @return
-   * @throws IOException
-   */
-  private String readResource(String resourcePath) throws IOException {
-    try (InputStream is = getClass().getResourceAsStream(resourcePath)) {
-      Reader reader = new InputStreamReader(is, "UTF-8");
-      StringWriter writer = new StringWriter();
-      char[] buf = new char[1024];
-      int len;
-      while ((len = reader.read(buf)) != -1) {
-        writer.write(buf, 0, len);
-      }
-      return writer.toString();
+  public void stop(Future<Void> done) {
+    if (this.cache != null) {
+      this.cache.close().subscribe(CompletableHelper.toObserver(done));
+    } else {
+      done.complete();
     }
   }
 
---
+++ current-line-updater/src/main/java/com/redhat/coderland/reactica/currentline/UserContinuousQueryListener.java
@@ -0,0 +1,57 @@
+package com.redhat.coderland.reactica.currentline;
+
+import com.redhat.coderland.reactica.model.User;
+import io.vertx.core.json.JsonArray;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.infinispan.query.api.continuous.ContinuousQueryListener;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UserContinuousQueryListener implements ContinuousQueryListener<String, User> {
+
+  private static final Logger LOGGER = LogManager.getLogger("UserContinuousQueryListener");
+  private static final String CURRENT_LINE_EVENTS = "current-line-events";
+
+  private final Vertx vertx;
+
+  private Map<String, User> current = new ConcurrentHashMap<>();
+
+  public UserContinuousQueryListener(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
+  private JsonObject generateOutput() {
+    JsonObject json = new JsonObject();
+    JsonArray queue = current.values().stream().map(JsonObject::mapFrom).collect(JsonArray::new, JsonArray::add, JsonArray::addAll);
+    return json.put("queue", queue);
+  }
+
+  private void fire() {
+    vertx.eventBus().send(CURRENT_LINE_EVENTS, generateOutput());
+  }
+
+  @Override
+  public void resultJoining(String key, User value) {
+    LOGGER.info("Received an new value for user with key " + key + " and value " + value);
+    current.put(key, value);
+    fire();
+  }
+
+  @Override
+  public void resultUpdated(String key, User value) {
+    LOGGER.info("Received an updated value for user with key " + key + " and value " + value);
+    current.put(key, value);
+    fire();
+  }
+
+  @Override
+  public void resultLeaving(String key) {
+    LOGGER.info("Received an removed value for user with key " + key);
+    current.remove(key);
+    fire();
+  }
+}
