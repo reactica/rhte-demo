b10df4dd05fb7b517ec8e29aa1915b00cc4c058f
Thomas Qvarnström <tqvarnst@redhat.com>
2018-09-24 16:49:45 +0200

    Made the deploy and create-application-config.sh location unaware.

--- setup/create-application-config.sh
+++ setup/create-application-config.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=$(dirname "$0")
+
+
 echo "Creating reactive-config config map"
-oc create configmap reactica-config --from-file=application.yaml
+oc create configmap reactica-config --from-file=${SCRIPT_DIR}/application.yaml
