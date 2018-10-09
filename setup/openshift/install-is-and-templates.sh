1db056f5315608c623ab7c4c67f7888478b527d8
jamesfalkner <schtool@gmail.com>
2018-10-09 13:30:31 -0400

    use previous imagestreams referencing registry.access.redhat.com to avoid need for docker login

--- setup/openshift/install-is-and-templates.sh
+++ setup/openshift/install-is-and-templates.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+DG_IS='https://raw.githubusercontent.com/jboss-container-images/jboss-datagrid-7-openshift-image/654af08e2837f0c85f9e8871c0d0178cc7eb3dc6/templates/datagrid72-image-stream.json'
+AMQ_IS='https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/amq-broker-71/amq-broker-7-image-streams.yaml'
+
 info "### INSTALLING IS FOR AMQ AND RDG"
 if $(oc get is/jboss-datagrid72-openshift -n openshift > /dev/null 2>&1); then
-      oc replace -f https://raw.githubusercontent.com/jboss-container-images/jboss-datagrid-7-openshift-image/datagrid72/templates/datagrid72-image-stream.json -n openshift --as=system:admin
+      oc replace -f $DG_IS -n openshift --as=system:admin
 else
-    oc create -f https://raw.githubusercontent.com/jboss-container-images/jboss-datagrid-7-openshift-image/datagrid72/templates/datagrid72-image-stream.json -n openshift --as=system:admin
+    oc create -f $DG_IS -n openshift --as=system:admin
 fi
 
 oc -n openshift import-image jboss-datagrid72-openshift:1.1 --as=system:admin > /dev/null && echo "jboss-datagrid72-openshift:1.1 image successfully imported"
 
 if $(oc get is/amq-broker-71-openshift -n openshift > /dev/null 2>&1); then
-    oc replace -f https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/amq-broker-71/amq-broker-7-image-streams.yaml -n openshift --as=system:admin
+    oc replace -f $AMQ_IS -n openshift --as=system:admin
 else
-    oc create -f https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/amq-broker-71/amq-broker-7-image-streams.yaml -n openshift --as=system:admin
+    oc create -f $AMQ_IS -n openshift --as=system:admin
 fi
 
-  oc -n openshift import-image jboss-amq-63:1.4 --as=system:admin > /dev/null && echo "jboss-amq-63:1.4 image successfully imported"
+  oc -n openshift import-image amq-broker-71-openshift:1.0 --as=system:admin > /dev/null && echo "amq-broker-71-openshift:1.0 image successfully imported"
 
 info "### INSTALLING TEMPLATES FOR AMQ AND RDG"
 
