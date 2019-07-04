657c66c499cf450a0966bfd152ca2867acca0e9b
Clement Escoffier <clement.escoffier@gmail.com>
2019-07-04 11:46:27 +0200

    Replace proton by the AMQP client

--- amqp-verticle/src/main/java/me/escoffier/reactive/amqp/AmqpVerticle.java
+++ amqp-verticle/src/main/java/me/escoffier/reactive/amqp/AmqpVerticle.java
@@ -1,94 +1,79 @@
 package me.escoffier.reactive.amqp;
 
-import io.vertx.core.Future;
+import io.reactivex.Completable;
+import io.reactivex.Single;
+import io.vertx.amqp.AmqpClientOptions;
+import io.vertx.amqp.AmqpReceiverOptions;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.json.JsonObject;
-import io.vertx.proton.*;
+import io.vertx.reactivex.amqp.*;
 import io.vertx.reactivex.core.AbstractVerticle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.qpid.proton.amqp.messaging.AmqpValue;
-import org.apache.qpid.proton.amqp.messaging.ApplicationProperties;
-import org.apache.qpid.proton.message.Message;
 
-
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class AmqpVerticle extends AbstractVerticle {
 
   private static final Logger LOGGER = LogManager.getLogger("AMQP Verticle");
 
   @Override
-  public void start(Future<Void> completion) {
+  public Completable rxStart() {
     LOGGER.info("Starting AMQP verticle");
     AmqpConfiguration configuration = config().mapTo(AmqpConfiguration.class);
-    ProtonClient client = ProtonClient.create(vertx.getDelegate());
-    Future<ProtonConnection> future = Future.future();
-    client.connect(configuration.getHost(), configuration.getPort(), configuration.getUser(), configuration.getPassword(),
-      connection -> {
-        if (connection.succeeded()) {
-          LOGGER.info("Connected to the AMQP broker {}", configuration.getHost());
-          future.complete(connection.result());
+    AmqpClient amqp = AmqpClient.create(vertx, new AmqpClientOptions()
+      .setHost(configuration.getHost())
+      .setPort(configuration.getPort())
+      .setUsername(configuration.getUser())
+      .setPassword(configuration.getPassword())
+      .setContainerId(configuration.getContainer())
+    );
+    return amqp.rxConnect()
+      .doOnSuccess(x -> LOGGER.info("Connected to the AMQP broker {}", configuration.getHost()))
+      .doOnError(err -> LOGGER.info("Unable to connect to AMQP broker {}", configuration.getHost(), err))
+      .flatMapCompletable(connection -> {
+        List<Single<?>> singles = new ArrayList<>();
+
+        LOGGER.info("Weaving event bus to AMQP connections");
+        // Start listeners to send event bus messages to AMQP
+        configuration.getEventBusToAmqp().forEach(bridge -> singles.add(setupAmqpSender(connection, bridge)));
+        LOGGER.info("Weaving AMQP to event bus connections");
+        // Start consumers to send AMQP messages to the event bus (AMQP -> Event Bus
+        configuration.getAmqpToEventBus().forEach(bridge -> singles.add(setupAmqpReceiver(connection, bridge)));
+        return Single.merge(singles).ignoreElements();
+      });
+  }
+
+  private Single<AmqpReceiver> setupAmqpReceiver(AmqpConnection connection, AmqpToEventBus bridge) {
+    LOGGER.info("AMQP {} => Event Bus {}", bridge.getQueue(), bridge.getAddress());
+    return connection.rxCreateReceiver(bridge.getQueue(), new AmqpReceiverOptions().setDurable(true),
+      message -> {
+        // Convert to headers.
+        JsonObject properties = message.applicationProperties();
+        JsonObject body = message.bodyAsJsonObject();
+        DeliveryOptions options = new DeliveryOptions();
+        properties.forEach(entry -> options.addHeader(entry.getKey(), entry.getValue().toString()));
+        if (bridge.isPublish()) {
+          vertx.eventBus().publish(bridge.getAddress(), body, options);
         } else {
-          LOGGER.info("Unable to connect to AMQP broker {}", configuration.getHost(), connection.cause());
-          future.fail(connection.cause());
+          vertx.eventBus().send(bridge.getAddress(), body, options);
         }
       });
+  }
 
-
-    future
-      .compose(connection -> {
-        LOGGER.info("Configuring connection...");
-        connection.setContainer(configuration.getContainer());
-        connection.open();
-        return Future.succeededFuture(connection);
-    })
-    .compose(connection -> {
-      LOGGER.info("Weaving event bus to AMQP connections");
-      // Start listeners to send event bus messages to AMQP
-      configuration.getEventBusToAmqp().forEach(bridge -> {
-        ProtonSender sender = connection.createSender(bridge.getQueue());
+  private Single<AmqpSender> setupAmqpSender(AmqpConnection connection, EventBusToAmqp bridge) {
+    return connection.rxCreateSender(bridge.getQueue())
+      .doOnSuccess(sender -> {
         LOGGER.info("Event Bus {} => AMQP {}", bridge.getAddress(), bridge.getQueue());
         vertx.eventBus().<JsonObject>consumer(bridge.getAddress(), msg -> {
-          Message message = Message.Factory.create();
-          message.setAddress(bridge.getQueue());
-          message.setBody(new AmqpValue(msg.body().encode()));
-          Map<String, Object> properties = new HashMap<>();
-          msg.headers().names().forEach(key -> properties.put(key, msg.headers().get(key)));
-          message.setApplicationProperties(new ApplicationProperties(properties));
-          sender.send(message);
+          AmqpMessageBuilder message = AmqpMessageBuilder.create().durable(true);
+          JsonObject headers = new JsonObject();
+          msg.headers().names().forEach(key -> headers.put(key, msg.headers().get(key)));
+          message.address(bridge.getQueue()).withJsonObjectAsBody(msg.body()).applicationProperties(headers);
+          sender.send(message.build());
         });
-        sender.open();
       });
-
-      return Future.succeededFuture(connection);
-    })
-    .compose(connection -> {
-      LOGGER.info("Weaving AMQP to event bus connections");
-      // Start consumers to send AMQP messages to the event bus (AMQP -> Event Bus
-      List<AmqpToEventBus> bridges = configuration.getAmqpToEventBus();
-      bridges.forEach(bridge -> {
-        LOGGER.info("AMQP {} => Event Bus {}", bridge.getQueue(), bridge.getAddress());
-        ProtonReceiver receiver = connection.createReceiver(bridge.getQueue());
-        receiver.handler((delivery, request) -> {
-          // Convert to headers.
-          ApplicationProperties properties = request.getApplicationProperties();
-          JsonObject json = new JsonObject((String) ((AmqpValue) request.getBody()).getValue());
-          DeliveryOptions options = new DeliveryOptions();
-          properties.getValue().forEach((key, value) -> options.addHeader(key, value.toString()));
-          if (bridge.isPublish()) {
-            vertx.eventBus().publish(bridge.getAddress(), json, options);
-          } else {
-            vertx.eventBus().send(bridge.getAddress(), json, options);
-          }
-        });
-        receiver.open();
-      });
-      return Future.succeededFuture();
-    })
-    .setHandler(res -> completion.handle(res.mapEmpty()));
   }
 
 }
